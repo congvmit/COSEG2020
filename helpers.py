ef2613aa70c5d53bb94ae36f5c27f96d84e0af7d
Cong Vo <congvm.it@gmail.com>
2020-11-10 15:09:32 +0900

    Fix helpers

--- helpers.py
+++ helpers.py
@@ -5,6 +5,7 @@ import os
 def save_args_to_file(args, save_folder):
     now = datetime.now()
     dt_string = now.strftime("config_%d-%m-%Y_%H:%M:%S.yaml")
+    os.makedirs(save_folder, exist_ok=True)
     path_to_save = os.path.join(save_folder, dt_string)
     with open(path_to_save, 'w') as f:
         yaml.dump(vars(args), f)
